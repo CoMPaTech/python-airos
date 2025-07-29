0207458d6e636157bc62a939785e059f538d03c2
Tom Scholten <git@scholten.nu>
2025-07-29 11:12:01 +0200

    Fixture generation for derived

---
+++ script/generate_ha_fixture.py
@@ -0,0 +1,27 @@
+"""Generate mock airos fixture for testing."""
+
+import json
+import logging
+import os
+import sys
+
+_LOGGER = logging.getLogger(__name__)
+
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root_dir = os.path.abspath(os.path.join(current_script_dir, os.pardir))
+
+if project_root_dir not in sys.path:
+    sys.path.append(project_root_dir)
+
+from airos.airos8 import AirOS, AirOSData  # noqa: E402
+
+# Define the path to save the fixture
+fixture_dir = os.path.join(os.path.dirname(__file__), "../fixtures")
+new_fixture_path = os.path.join(fixture_dir, "airos_ap-ptp.json")
+base_fixture_path = os.path.join(fixture_dir, "ap-ptp.json")
+
+with open(base_fixture_path) as source, open(new_fixture_path, "w") as new:
+    source_data = json.loads(source.read())
+    derived_data = AirOS.derived_data(None, source_data)
+    new_data = AirOSData.from_dict(derived_data)
+    new.write(json.dumps(new_data.to_dict()))
