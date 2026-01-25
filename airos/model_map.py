22b1ea7172d3a14129590539ea81de4867aa45e7
Eloi Codina <eloi.codina@gmail.com>
2026-01-25 15:32:14 +0100

    add LBE-5AC-23

--- airos/model_map.py
+++ airos/model_map.py
@@ -82,6 +82,7 @@ SITE_MODELS: dict[str, str] = {
 MANUAL_MODELS: dict[str, str] = {
     "LiteAP AC": "LAP-120",  # Shortened name for airMAX Lite Access Point AC, Issue 137
     "LiteAP GPS": "LAP-GPS",  # Shortened name for airMAX Lite Access Point GPS
+    "LiteBeam 5AC 23": "LBE-5AC-23",
     "NanoStation loco M5": "LocoM5",  # XM firmware version 6 - note the reversed names
 }
 
